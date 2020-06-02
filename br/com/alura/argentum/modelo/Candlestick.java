8f9452aa3ef3604d4ebafd5b2189db48c370562a
Marina Pelissari <marinapelissari26@gmail.com>
2020-06-02 19:28:43 -0300

    Adiciona classe Candlestick e seus métodos

---
+++ br/com/alura/argentum/modelo/Candlestick.java
@@ -0,0 +1,54 @@
+package br.com.alura.argentum.modelo;
+
+import java.time.LocalDateTime;
+
+public class Candlestick {
+    private final double abertura;
+    private final double fechamento;
+    private final double minimo;
+    private final double maximo;
+    private final double volume;
+    private final LocalDateTime data;
+
+    public Candlestick(double abertura, double fechamento, double minimo, double maximo, double volume,
+            LocalDateTime data) {
+        this.abertura = abertura;
+        this.fechamento = fechamento;
+        this.minimo = minimo;
+        this.maximo = maximo;
+        this.volume = volume;
+        this.data = data;
+    }
+
+    public double abertura() {
+        return abertura;
+    }
+
+    public double fechamento() {
+        return fechamento;
+    }
+
+    public double minimo() {
+        return minimo;
+    }
+
+    public double maximo() {
+        return maximo;
+    }
+
+    public double volume() {
+        return volume;
+    }
+
+    public LocalDateTime data() {
+        return data;
+    }
+
+    public boolean isAlta() {
+        return this.abertura < this.fechamento;
+    }
+
+    public boolean isBaixa() {
+        return this.abertura > this.fechamento;
+    }
+}
