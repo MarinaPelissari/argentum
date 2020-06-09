35124c89f594e785213399da8755dfa2405aae43
Marina Pelissari <marinapelissari26@gmail.com>
2020-06-09 18:06:10 -0300

    Utiliza padrão builder para gerar Candlestick e adiciona método

---
+++ br/com/alura/argentum/testes/CandlestickTest.java
@@ -0,0 +1,20 @@
+package br.com.alura.argentum.testes;
+
+import java.time.LocalDateTime;
+
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import br.com.alura.argentum.modelo.CandleBuilder;
+import br.com.alura.argentum.modelo.Candlestick;
+
+public class CandlestickTest {
+    @Test
+    public void test() {
+        CandleBuilder builder = new CandleBuilder();
+
+        Candlestick candle = builder.comAbertura(10.0).comFechamento(20.0).comMinimo(10.0).comMaximo(20.0)
+                .comVolume(100.0).comData(LocalDateTime.now()).geraCandle();
+    }
+}
